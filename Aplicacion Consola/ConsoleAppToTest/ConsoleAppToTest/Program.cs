72ba0ca87ac0b384643651fceec9071533e1a29b
Alexis-Chavarria <johan_chavarria@cun.edu.co>
2024-02-01 09:30:40 -0500

    first commit

---
+++ Aplicacion Consola/ConsoleAppToTest/ConsoleAppToTest/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+string ejemplo = "Esto es una cadena con \"comillas dobles\".";
+Console.WriteLine(ejemplo);
+
+
+string htmlTemplate = @"<!DOCTYPE html>
+<html lang=""es"">
+<head>
+    <meta charset=""UTF-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+    <style>
+        /* ... Estilos CSS ... */
+    </style>
+</head>
+<body>
+    <div class=""container"">
+            <img class=""banner-img"" src=""https://repocun.s3.us-east-2.amazonaws.com/fabrica_software/correo_arl/header-mailing.png"" alt=""Banner"" />
+
+        <!-- ... Resto del HTML ... -->
+    </div>
+</body>
+</html>";
+
+Console.WriteLine(htmlTemplate);
+
+
+
---
+++ Aplicacion Consola/QuestPDF/QuestPDF/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Configurar la licencia
+//QuestPDF.Settings.License = QuestPDF.Infraestructure.LiceseType.Community;
+
+
+Console.WriteLine("Hello, World!");
+
+
